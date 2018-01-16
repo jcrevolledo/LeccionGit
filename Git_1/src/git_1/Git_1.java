860afa0d3d91122e90b90a0c00e992af22e0b49f
jcrevolledo <jcrevolledo@hotmail.com>
2018-01-16 11:46:10 -0500

    Cambio desde Web
    El primer cambio

--- Git_1/src/git_1/Git_1.java
+++ Git_1/src/git_1/Git_1.java
@@ -17,5 +17,7 @@ public class Git_1 {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+    private void DesdeGitWeb() {
+    // Metodo nuevo
+    }
 }
